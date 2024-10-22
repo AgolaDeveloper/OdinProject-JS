a1117f4f6ad6e7db4eb82ffec910a8c3208cabc7
AgolaDeveloper <owinoagola00@gmail.com>
2024-10-22 21:44:10 +0300

    Add event-handler in js script

---
+++ script.js
@@ -0,0 +1,11 @@
+//this is the js file that gives this site its real-time functionality
+
+//Event-handlers
+//#event-listeners
+
+const nav=document.getElementById("navbar");
+nav.addEventListener(MouseEvent, alerrt);
+
+function alerrt(){
+    alert("This is the site's navigation bar")
+}
